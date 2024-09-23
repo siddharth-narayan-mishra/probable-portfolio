a08495670483d27e2873fe58c7d18f52b382391b
Aditya Patro <92665963+adityathenerd@users.noreply.github.com>
2024-09-23 15:00:41 +0530

    chore: Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import Clients from "@/components/Clients";
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
+import ExtraCurriculars from "@/components/ExtraCurriculars";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
@@ -20,7 +21,7 @@ const Home = () => {
         <Experience />
         <RecentProjects />
         <Clients />
-        <Clients />
+        <ExtraCurriculars />
         <Approach />
         <Footer />
       </div>
