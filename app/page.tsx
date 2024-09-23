e09ab717051cabae808c6820fc86ec1abc4fedef
Aditya Patro <92665963+adityathenerd@users.noreply.github.com>
2024-09-23 14:37:55 +0530

    chore: fixed website structure

--- app/page.tsx
+++ app/page.tsx
@@ -17,10 +17,10 @@ const Home = () => {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
-        <Grid />
+        <Experience />
         <RecentProjects />
         <Clients />
-        <Experience />
+        <Clients />
         <Approach />
         <Footer />
       </div>
